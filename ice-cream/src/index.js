6a2f82e1e1f6fc25d5b325a7238ae62c53a63272
MaximKalinin <maxim.kalinin.work@gmail.com>
2021-05-16 14:18:08 +0200

    fix(ice-cream): fixed default value for subscribe

--- ice-cream/src/index.js
+++ ice-cream/src/index.js
@@ -2,6 +2,8 @@ import {mount, unmount} from './root';
 import {store} from './store';
 
 function subscribe(listener) {
+    listener(store.getState());
+
     return store.subscribe(() => listener(store.getState()))
 }
 
