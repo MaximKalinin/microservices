f7d5e950863574582014104a08add4f6b0881d2b
MaximKalinin <maxim.kalinin.work@gmail.com>
2021-05-16 14:22:33 +0200

    feat(app): add build script

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.jsx'),
     output: { path: path.join(__dirname, 'dist'), filename: 'index.js' },
-    mode: 'development',
     resolve: { extensions: ['.js', '.jsx'] },
     devServer: { contentBase: path.join(__dirname), open: true },
     module: {
